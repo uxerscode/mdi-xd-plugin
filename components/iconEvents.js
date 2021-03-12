5f84b8af21b829d52dc74a377c2a852fc40d0758
Larry Weinheimer <larry.weinheimer@aveva.com>
2021-03-11 17:23:51 -0800

    Working version

--- components/iconEvents.js
+++ components/iconEvents.js
@@ -3,39 +3,39 @@ const { search } = require("./search");
 const { createIcon } = require("./createIcon");
 const { centerInViewport } = require("../lib/utils");
 
-let _iconList = null;
-
 function setIconsEvents(panel) {
     // Search
     const iconSearch = panel.querySelector('#icon-search-text');
     const searchBtn = panel.querySelector('#icon-search');
     searchBtn.addEventListener('click', () => {
         search(panel, iconSearch.value);
-    })
+    });
+    panel.addEventListener('keydown', event => {
+        if(event.keyCode === 13) {
+            search(panel, iconSearch.value);
+        }
+    });
     // // Icon List
-    // const list = panel.querySelector('.icons--list');
-    // list.addEventListener('click', event => {
-    //     if (event) {
-    //         let target = event.target;
-    //         while (target.nodeName != 'LI') {
-    //             target = target.parentNode;
-    //             if (target === null) {
-    //                 break
-    //             }
-    //         }
-    //         if (target !== null) {
-    //             const classes = target.classList;
-    //             let name = target.classList._cachedAttrValue;
-    //             name = name.split(' ')[1];
-    //             application.editDocument((selection) => {
-    //                 let icon = document.querySelector('.' + name);
-    //                 let path = icon.getAttribute('data-icon-path');
-    //                 let newIcon = createIcon(selection, name, path);
-    //                 centerInViewport(selection, newIcon);
-    //             });
-    //         }
-    //     }
-    // });
+    const list = panel.querySelector('#icons--list');
+    list.addEventListener('click', event => {
+        if (event) {
+            let target = event.target;
+            while (target.nodeName != 'LI') {
+                target = target.parentNode;
+                if (target === null) {
+                    break
+                }
+            }
+            if (target !== null) {
+                application.editDocument((selection) => {
+                    let name = target.getAttribute('name');
+                    let path = target.getAttribute('path');
+                    let newIcon = createIcon(selection, name, path);
+                    centerInViewport(selection, newIcon);
+                });
+            }
+        }
+    });
 }
 
 module.exports = {
--- components/search.js
+++ components/search.js
@@ -1,29 +1,37 @@
 const { icons } = require('./icons');
+const { iconItem } = require('./iconItem');
 
 
 function search(panel, search) {
-    const _iconList = panel.querySelectorAll('#icons .icons--item');
-    const _warn = panel.querySelector("#warn");
-    const _err = panel.querySelector("#err");
+    for(var i=0; i < 25; i++){
+        const _icon = panel.querySelector('#icon-' + i);
+        _icon.classList.remove('show');
+        _icon.classList.add('hide');
+    }
     var total = 0;
     let lookFor = new RegExp(search.toLowerCase());
-    icons.forEach(icon => {
+    for(var i=0; i < icons.length; i++) {
+        const icon = icons[i];
         if (lookFor.test(icon.search)) {
+            if(total < 25) {
+                iconItem(panel, icon, total);
+            }
             total++;
         }
-    });
+    }
+    const _err = panel.querySelector("#err");
     if (total === 0) {
         _err.classList.remove('hide');
         _err.classList.add('show');
+        _err.innerHTML = 'None found';
     } else
     if (total > 25) {
-        _warn.classList.remove('hide');
-        _warn.classList.add('show');
-        _err.classList.remove('show');
+        _err.classList.remove('hide');
+        _err.classList.add('show');
+        _err.innerHTML = `25 of ${total} shown`;
     } else {
-        _warn.classList.remove('show');
-        _warn.classList.add('hide');
         _err.classList.remove('show');
+        _err.classList.add('hide');
     }
 }
 
